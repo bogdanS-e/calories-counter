899f7f6d79b29921631668d99ffa74fe351b0a65
bogdanS-e <61601182+bogdanS-e@users.noreply.github.com>
2021-11-23 20:48:12 +0200

    init base components

---
+++ components/signIn.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Button } from 'react-native-elements';
+
+const SingIn = () => {
+  return (
+    <Button title="SingIn" />
+  );
+}
+
+export default SingIn;
---
+++ components/signUp.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Button } from 'react-native-elements';
+
+const SingUp = () => {
+  return (
+    <Button title="SingUp" />
+  );
+}
+
+export default SingUp;
