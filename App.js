a1ecf6faee1f6716685a1751b7cbca8d9734c75f
ivan.k <ivan.k@code-care.pro>
2021-11-23 20:50:50 +0200

    routing

--- App.js
+++ App.js
@@ -1,17 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Button } from 'react-native-elements';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { StyleSheet, Text, View } from "react-native";
+import { Button } from "react-native-elements";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <SafeAreaProvider>
-       <View style={styles.container}>
-        <Text>Open up </Text>
-        <Button title="Button" />
-        <StatusBar style="auto" />
-      </View>
+      <NavigationContainer>
+        <View style={styles.container}>
+          <Text>Open upf </Text>
+          <Button title="Button" />
+          <StatusBar style="auto" />
+        </View>
+      </NavigationContainer>
     </SafeAreaProvider>
   );
 }
@@ -19,8 +25,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'red',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "red",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
