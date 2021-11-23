eadb760bfa71f598d575c5a3a437882da393f7b1
bogdanS-e <61601182+bogdanS-e@users.noreply.github.com>
2021-11-23 21:28:31 +0200

    init signIn form

--- components/signIn.jsx
+++ components/signIn.jsx
@@ -1,10 +1,57 @@
 import React from "react";
 import { Button } from "react-native-elements";
+import { StyleSheet, Text, View } from "react-native";
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { Input } from 'react-native-elements';
 
 const SingIn = ({ navigation }) => {
   return (
-    <Button title="SingIn" onPress={() => navigation.navigate("signUp")} />
+    <View style={styles.container}>
+      <Text style={styles.text}>Sign In</Text>
+      <Input
+        style={styles.input}
+        placeholder='User name'
+        leftIcon={
+          <Icon
+            name='user'
+            size={24}
+            color='black'
+          />
+        }
+      />
+      <Input
+        style={styles.input}
+        placeholder="Password"
+        leftIcon={
+          <Icon
+            name='key'
+            size={24}
+            color='black'
+          />
+        }
+      />
+      <Button title="SingIn" onPress={() => navigation.navigate("signUp")} />
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  text: {
+    textAlign: 'center',
+    fontSize: 48,
+    marginBottom: 20,
+  },
+  input: {
+    fontSize: 18,
+    maxWidth: 200,
+  },
+  container: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
+    marginTop: -150,
+  }
+});
 
 export default SingIn;
