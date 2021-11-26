5caf24d534a0c4e7e7e4d1756a21abf62813dd75
bogdanS-e <61601182+bogdanS-e@users.noreply.github.com>
2021-11-26 18:24:23 +0200

    add categories

--- components/Food.jsx
+++ components/Food.jsx
@@ -1,11 +1,100 @@
-import React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import React, { useMemo, useState } from "react";
+import { StyleSheet, View, Text, TextInput, ScrollView, Image, Dimensions } from "react-native";
 import NavBar from "./NavBar";
 
+const ScreenWidth = Dimensions.get("window").width;
+
 const Food = ({ navigation, route }) => {
+  const [categories] = useState([
+    {
+      imageUrl: 'https://www.burpee.com/media/catalog/category/POT-Cat1.jpg',
+      name: 'pinaple',
+      calories: '222ccal / 100g',
+      id: '222ccal / 100g',
+    },
+    {
+      imageUrl: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/marrot-1561576540.jpg?crop=0.443xw:0.786xh;0.277xw,0.0960xh&resize=480:*',
+      name: 'potatoe',
+      calories: '222ccal / 100g',
+      id: '222ccal / 100g4',
+    },
+    {
+      imageUrl: 'https://learnenglishfunway.com/wp-content/uploads/2020/12/Vegetables.jpg',
+      name: 'onion',
+      calories: '222ccal / 100g',
+      id: '222ccal / 100g3',
+    },
+    {
+      imageUrl: 'https://image.freepik.com/free-photo/top-view-assortment-vegetables-paper-bag_23-2148853335.jpg',
+      name: 'gurlic',
+      calories: '222ccal / 100g',
+      id: '222ccal / 100g2',
+    },
+    {
+      imageUrl: 'https://www.burpee.com/media/catalog/category/POT-Cat1.jpg',
+      name: 'bla bla bla',
+      calories: '222ccal / 100g',
+      id: '222ccal / 100g1',
+    },
+    {
+      imageUrl: 'https://www.burpee.com/media/catalog/category/POT-Cat1.jpg',
+      name: 'bla bla bla',
+      calories: '222ccal / 100g',
+      id: '222ccal / 100g15',
+    },
+    {
+      imageUrl: 'https://www.burpee.com/media/catalog/category/POT-Cat1.jpg',
+      name: 'bla bla bla',
+      calories: '222ccal / 100g',
+      id: '222ccal / 100g14',
+    },
+    {
+      imageUrl: 'https://www.burpee.com/media/catalog/category/POT-Cat1.jpg',
+      name: 'bla bla bla',
+      calories: '222ccal / 100g',
+      id: '222ccal / 100g13',
+    },
+  ]);
+
+  const [searchText, setSearchText] = useState('');
+
+  const filtredCategories = useMemo(() => {
+    if (searchText.trim() === '') {
+      return categories;
+    }
+
+    if (!Array.isArray(categories)) {
+      return [];
+    }
+
+    return categories.filter((category) => category.name.includes(searchText.trim().toLocaleLowerCase()));
+  }, [searchText]);
+
   return (
     <View style={styles.container}>
-      <Text>Food</Text>
+      <Text style={styles.header}>Food</Text>
+      <View style={styles.mainInfo}>
+        <TextInput style={styles.search} placeholder='Search' onChangeText={(newText) => setSearchText(newText)} />
+      </View>
+      <View style={styles.scrollWrapper}>
+        <ScrollView contentContainerStyle={styles.scrollView}>
+          {filtredCategories.length ? (
+              filtredCategories.map(({ id, name, calories, imageUrl }) => (
+              <View style={styles.card} key={id}>
+                <Image style={styles.image} source={{ uri: imageUrl }} />
+                <View style={styles.cardText} key={id}>
+                  <Text style={styles.cardName}>{name}</Text>
+                  <Text style={styles.cardCalories}>{calories}</Text>
+                </View>
+              </View>
+            ))
+            ) : (<>
+              <Text style={styles.noResult}>There are no categories that contain "{searchText}"</Text>
+              <Image style={styles.image2} source={require("../assets/avocado2.png")} />
+            </>)
+          }
+        </ScrollView>
+      </View>
       <NavBar navigation={navigation} route={route} />
     </View>
   );
@@ -18,6 +107,160 @@ const styles = StyleSheet.create({
     justifyContent: "space-between",
     position: "relative",
     backgroundColor: "#F4F4F4",
+  },
+  card: {
+    width: ScreenWidth - 80,
+    marginBottom: 15,
+    backgroundColor: '#ffffff',
+    borderRadius: 14,
+    paddingBottom: 7,
+  },
+  noResult: {
+    textAlign: 'center',
+    fontSize: 18,
+    marginTop: 40,
+
+  },
+  cardText: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  cardName: {
+    fontSize: 22,
+    color: 'rgba(0,0,0,0.6)',
+    marginVertical: 10,
+    paddingLeft: 8,
+    textTransform: 'capitalize',
+  },
+  cardCalories: {
+    fontSize: 18,
+    color: 'rgba(0,0,0,0.4)',
+    marginVertical: 10,
+    paddingRight: 8,
+  },
+  scrollView: {
+    paddingHorizontal: 40,
+    display: "flex",
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+  },
+  header: {
+    marginTop: 40,
+    marginLeft: 30,
+    fontWeight: "bold",
+    fontSize: 24,
+  },
+  image: {
+    height: 200,
+    borderTopRightRadius: 14,
+    borderTopLeftRadius: 14,
+  },
+  image2: {
+    marginTop: 50,
+    width: 150,
+    height: 250,
+  },
+  mainInfo: {
+    marginTop: 15,
+    marginHorizontal: 30,
+    backgroundColor: "white",
+    height: 40,
+    borderRadius: 15,
+    overflow: 'hidden',
+  },
+  search: {
+    height: 40,
+    padding: 10,
+    paddingHorizontal: 15,
+    fontSize: 18,
+  },
+  horizontal: {
+    alignItems: "center",
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  progressBar: {
+    backgroundColor: "#DCDFE6",
+    height: 10,
+    borderRadius: 5,
+    marginVertical: 20,
+    position: "relative",
+  },
+  progressBarCenter: {
+    backgroundColor: "#4065A9",
+    borderRadius: 5,
+    height: 10,
+    top: 0,
+    left: 0,
+    width: 100,
+    position: "absolute",
+  },
+  text1: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "#3B3B3B",
+  },
+  text2: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "#3B3B3B",
+  },
+  text3: {
+    fontSize: 12,
+    color: "#BCBCBC",
+  },
+  scrollWrapper: {
+    flex: 1,
+    height: 200,
+    marginTop: 20,
+  },
+  foodBlock: {
+    marginVertical: 5,
+    marginHorizontal: 40,
+  },
+  foodIcon: {
+    minHeight: 30,
+    minWidth: 30,
+    backgroundColor: "#FDFEF8",
+    borderRadius: 15,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  foodIconText: {
+    fontSize: 20,
+    color: "#ABD638",
+  },
+  foodGradientBlock: {
+    flexDirection: "row",
+    padding: 10,
+    padding: 15,
+    height: 80,
+    borderRadius: 8,
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  foodBlockTitle: {
+    color: "#EBE9E9",
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  foodBlockRec: {
+    color: "#EBE9E9",
+    fontSize: 16,
+  },
+  foodBlockNow: {
+    backgroundColor: "#7DAA59",
+    minWidth: 70,
+    minHeight: 30,
+    borderRadius: 15,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  foodBlockNowText: {
+    color: "#EBE9E9",
   },
 });
 
